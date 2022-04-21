184ff47d84785f431aafc27c9522cb9234c2ca33
Fiona <fhehlen@ethz.ch>
2022-04-21 22:13:46 +0200

    threading is almost good

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,13 +1,14 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.button import Button
 
-class GUI_GridLayout(GridLayout):
+from server import GetVoltage
 
+
+class GUI_GridLayout(GridLayout):
     voltage_label_1 = StringProperty("Voltage 1?")
     voltage_label_2 = StringProperty("Voltage 2?")
 
@@ -20,13 +21,17 @@ class GUI_GridLayout(GridLayout):
     voltage_output_1 = NumericProperty(0)
     voltage_output_2 = NumericProperty(0)
 
+    server: GetVoltage
+
     def press_1(self):
         voltage_1 = self.voltage_input_1.text
-        self.voltage_output_1  = float(voltage_1)
+        self.voltage_output_1 = float(voltage_1)
+        self.server.voltage_1 = float(voltage_1)
 
     def press_2(self):
         voltage_2 = self.voltage_input_2.text
         self.voltage_output_2 = float(voltage_2)
+        self.server.voltage_2 = float(voltage_2)
 
     def build(self):
         self.add_widget(Label(text=self.voltage_label_1))
@@ -44,16 +49,24 @@ class GUI_GridLayout(GridLayout):
         self.add_widget(self.submit_button_2)
 
 
-
 class apason_GUIApp(App):
+    server = None
+
+    def setServer(self, server):
+        self.server = server
+
     def build(self):
-        return GUI_GridLayout()
+        layout = GUI_GridLayout()
+        layout.server = self.server
+        return layout
+
+    def on_stop(self):
+        self.server.stop()
+
 
 # class GUI_Window(Widget):
 #     def build(self):
 #         return GUI_GridLayout(voltage='Voltage ?')
-
-
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,13 +1,36 @@
 import talktoArduino as talk
 import configurations as conf
 import sensors as sens
-import kivy as UI
+import GUI.GUI as gui
+from threading import Thread
+import time
+
+
+class GetVoltage:
+    p:Thread
+    def __init__(self):
+        self.voltage_1 = 0
+        self.voltage_2 = 0
+        self.p = Thread(target=self.run)
+        self.p.start()
+
+    def fetchVoltage(self, interface):
+        self.voltage_1 = interface.voltage_output_1
+        self.voltage_2 = interface.voltage_output_2
+
+    def run(self):
+        while (True): #TODO: add condition to leave the while loop
+            print(self.voltage_1)
+            print(self.voltage_2)
+
+            time.sleep(1)
+
+    def stop(self):
+        self.p.join()
 
 
 if __name__ == '__main__':
-
-    arduinos = talk.talktoArduino()
-
-    check_configurations = conf.check_configurations
-    set_configurations = conf.set_configurations
-
+    view: gui.apason_GUIApp = gui.apason_GUIApp()
+    voltage = GetVoltage()
+    view.setServer(voltage)
+    view.run()

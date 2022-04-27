0461e583832ad72cb45b45e59a6e3eada2c391c3
Fiona <fhehlen@ethz.ch>
2022-04-27 13:58:37 +0200

    functional lock implemented!

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import ArduinoCommunication as ard_com
 import GUI.GUI as gui
-from threading import Thread
+from threading import Thread, Lock
 import time
 import sensors
 import Sensor_Update_List as ulist
@@ -12,13 +12,13 @@ class Command_Center:
     command_sender_thread: Thread
     run = True
 
-    def __init__(self, arduino, interface):
+    def __init__(self, arduino, interface, lock):
         self.voltage_1 = 0.0
         self.voltage_2 = 0.0
         self.arduino: ard_com.ArduinoCommunication = arduino
         self.interface: gui.apason_GUIApp = interface
 
-        self.command_sender_thread = Thread(target=self.run)
+        self.command_sender_thread = Thread(target=self.run, args=(lock,))
         self.command_sender_thread.start()
 
 
@@ -34,14 +34,15 @@ class Command_Center:
     def stop_server(self):
         self.run = False
 
-    def run(self):
+    def run(self, lock):
         while (self.run):
-
+            lock.acquire()
             if(self.fetchVoltage()):
                 self.arduino.sendVoltage(1, self.voltage_1, 'A')
                 self.arduino.sendVoltage(1, self.voltage_2, 'B')
-
+            lock.release()
             time.sleep(1)
+
 
     def stop(self):
         self.command_sender_thread.join()
@@ -51,25 +52,25 @@ class Update_List:
     update_list_thread: Thread
     run = True
 
-    def __init__(self, interface):
+    def __init__(self, interface, lock):
         self.interface: gui.apason_GUIApp = interface
         self.list = ulist.Sensor_Update_List()
 
-        self.update_list_thread = Thread(target=self.run)
+        self.update_list_thread = Thread(target=self.run, args=(lock,))
         self.update_list_thread.start()
 
     def set_from_list(self):
         self.interface.pressure_display_1 = str(self.list.pressure[0].current_value)
 
 
-    def run(self):
+    def run(self, lock):
         while (self.run):
-
-            sensor.updateValue(arduino.retrieveMeasurement(1, sensors.pressure_sensors[0]))
+            lock.acquire()
+            self.list.pressure[0].updateValue(arduino.retrieveMeasurement(1, sensors.pressure_sensors[0]))
 
             #if not len(self.list.pressure) == 0:
             self.set_from_list()
-
+            lock.release()
             time.sleep(1)
 
     def stop_server(self):
@@ -81,29 +82,16 @@ class Update_List:
 
 if __name__ == '__main__':
 
+    arduino_lock = Lock()
+
     arduino = ard_com.ArduinoCommunication()
 
     sensors = sensors.Arduino_Sensors()
 
     update_list = ulist.Sensor_Update_List()
 
-    print(len(sensors.pressure_sensors))
-    print(len(update_list.pressure))
-
-    print(sensors.pressure_sensors)
-    print(update_list.pressure)
-
-    index = 0
-
-    for sensor in update_list.pressure:
-        sensor.updateValue(arduino.retrieveMeasurement(1, sensors.pressure_sensors[index]))
-        index += 1
-
-    for sensor in update_list.pressure:
-        print(sensor.current_value)
-
     view: gui.apason_GUIApp = gui.apason_GUIApp()
-    voltage = Command_Center(arduino, view)
-    pressure = Update_List(view)
+    voltage = Command_Center(arduino, view, arduino_lock)
+    pressure = Update_List(view, arduino_lock)
     view.setServer(voltage, pressure)
     view.run()
